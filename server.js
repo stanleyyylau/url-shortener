8f19a9698a9f738e01f780197e6ccafbc3919773
Stanley <stanleyyylau@gmail.com>
2016-09-13 10:02:57 +0800

    finish basic configuration

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 5000;
+
 app.get('/new/:url*', function(req, res){
   console.log(req.originalUrl);
 })
 
-app.listen('5000', function(){
+app.listen(port, function(){
   console.log('Server is on...');
 });
